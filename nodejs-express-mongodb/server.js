38481c8f91699b22058b2109c34b1a5b61c3f222
Michael <michaelgrant13@hotmail.co.uk>
2021-03-16 09:54:21 +0000

    First Commit

---
+++ nodejs-express-mongodb/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the MEVN Tutorial Program" });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+/* 
+Express is for building the Rest apis
+body-parser helps to parse the request and create the req.body object
+cors provides Express middleware to enable CORS with various options.
+– create an Express app, then add body-parser and cors middlewares using app.use() method. Notice that we set origin: http://localhost:8081.
+– define a GET route which is simple for test.
+– listen on port 8080 for incoming requests.
+*/
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
---
+++ vue-js-client-crud/vue-js-client-crud/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center mr-2">
+        Tutorial Storage Vault
+      </div>
+      <v-spacer></v-spacer>
+      
+      <v-btn to="/tutorials" text>
+        Tutorial List
+      </v-btn>
+
+      <v-btn to="/add" text>
+        Add Tutorial
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
